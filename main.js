8b373b22480f22813596ef3f3eb341615a5d4b0e
TMorganR <105664184+TMorganR@users.noreply.github.com>
2022-11-27 13:34:18 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,132 @@
 // │ Your code here! │
 // └─────────────────┘
 
+/*
+functionName: createUser
+parameters: 2 strings (firstName, lastName)
+
+return: object
+{
+    firstName: string
+    lastName: string
+} 
+
+
+
+*/
+
+function createUser(firstName, lastName){
+    let user = {
+        firstName,
+        lastName,
+    }
+    return user
+}
+
+console.log(createUser('Tyler', 'Morgan'))
+console.log(createUser('John', 'Wick'))
+
+function setAge(user, age){
+    user.age = age
+    return user
+}
+
+console.log(setAge(createUser('Tyler', 'Morgan'), 25))
+console.log(setAge({firstName: 'Tyler', lastName: 'Morgan'}, 25))
+
+
+function incrementAge(user){
+    user.age = user.age + 1
+    return user
+}
+
+console.log(incrementAge(createUser('Tyler', 'Morgan')))
+console.log(incrementAge({firstName: 'Tyler', lastName: 'Morgan'}))
+
+
+function fixCar(car){
+    car.needsMaintence = false
+    return car
+}
+
+console.log(fixCar(
+{
+    make: 'Mazda',
+    model: 'RX8',
+    Year: 2008,
+    needsMaintence: true
+}))
+
+
+function addGrades(student, newGrades){
+    
+    for(let i = 0; i < newGrades.length; i++){
+        student.grades.push(newGrades[i])
+    }
+
+    return student
+}
+
+let student = {
+    name: 'Tyler Morgan',
+    email: 'Tylermorgan@codeimmersives.com',
+    grades: [80]
+}
+
+let newGrades = [88, 90, 75, 85, 94, 80, 70, 91, 78]
+
+console.log(addGrades(student, newGrades))
+
+
+function getDataType(object, key){
+    let keytype = key
+    keytype = typeof object[key]
+    return keytype
+}
+
+console.log(getDataType)
+
+
+function addTodo(todo, newTodo){
+    todo.push(newTodo)
+    return todo
+}
+
+const playlist = {
+    title: 'Vibez',
+    duration: 5,
+    songs: [
+        {
+            title: 'Evil Fantasy',
+            artist: 'Freddie Dredd',
+            duration: 2,
+        },
+        {
+            title: 'Money & Love',
+            artist: 'Wiz Kid',
+            duration: 3
+        }
+    ]
+}
+
+function newSong(title, artist, duration){
+    const song = {
+        title: title,
+        artist: artist,
+        duration: duration
+    }
+    return song
+}
+
+function addSong(playlist, song){
+    playlist.duration = playlist.duration + song.duration
+    playlist.song.push(song)
+    return playlist
+}
+
+
+
+
 
 
 
